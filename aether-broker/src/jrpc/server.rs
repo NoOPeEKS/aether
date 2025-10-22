099ee7f59a6eb8ca9a639b9660c7e4a48da46a6a
NoOPeEKS <arnauapps@gmail.com>
2025-10-22 21:05:40 +0200

    feat: register worker rpc method and hashset

--- aether-broker/src/jrpc/server.rs
+++ aether-broker/src/jrpc/server.rs
@@ -1,26 +1,40 @@
 use crate::jrpc::protocol::{JsonRpcNotification, JsonRpcRequest, JsonRpcResponse};
 use crate::state::BrokerState;
+use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
+use std::sync::Arc;
 use tokio::io::AsyncReadExt;
 use tokio::io::{AsyncBufReadExt, BufReader};
 use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::RwLock;
 use tracing::info;
 
 pub async fn create_jrpc_server(state: BrokerState, port: usize) {
+    let state = Arc::new(state);
     let listener = TcpListener::bind(format!("0.0.0.0:{port}"))
         .await
         .unwrap_or_else(|_| panic!("Could not bind JRPC server to 0.0.0.0:{port}"));
 
+    let workers: HashSet<String> = HashSet::new();
+    let worker_list = Arc::new(RwLock::new(workers));
+
     loop {
         if let Ok((stream, addr)) = listener.accept().await {
             info!("[INFO] Accepted TCP connection from {}", addr);
-            tokio::spawn(handle_jrpc_connection(stream));
+            let state = Arc::clone(&state);
+            let workers = Arc::clone(&worker_list);
+            tokio::spawn(handle_jrpc_connection(stream, state, workers));
         } else {
-            info!("[INFO] Could not accept an incoming connection from");
+            info!("[INFO] Could not accept an incoming connection");
         }
     }
 }
 
-async fn handle_jrpc_connection(stream: TcpStream) {
+async fn handle_jrpc_connection(
+    stream: TcpStream,
+    state: Arc<BrokerState>,
+    workers: Arc<RwLock<HashSet<String>>>,
+) {
     let mut reader = BufReader::new(stream);
     loop {
         let mut line = String::new();
@@ -58,7 +72,8 @@ async fn handle_jrpc_connection(stream: TcpStream) {
                 continue;
             }
 
-            match process_jsonrpc_message(&message_body).await {
+            let workers = Arc::clone(&workers);
+            match process_jsonrpc_message(&message_body, &state, workers).await {
                 Ok(_) => {}
                 Err(_) => continue,
             }
@@ -69,17 +84,35 @@ async fn handle_jrpc_connection(stream: TcpStream) {
     }
 }
 
-async fn process_jsonrpc_message(message: &[u8]) -> anyhow::Result<()> {
+#[derive(Serialize, Deserialize, Debug)]
+struct RegisterWorkerRequest {
+    worker_id: String,
+}
+
+async fn process_jsonrpc_message(
+    message: &[u8],
+    state: &BrokerState,
+    workers: Arc<RwLock<HashSet<String>>>,
+) -> anyhow::Result<()> {
     let message = String::from_utf8_lossy(message);
     let message: serde_json::Value = serde_json::from_str(&message)?;
     if message.get("id").is_some() {
         // It's a request
         let request: JsonRpcRequest = serde_json::from_value(message)?;
-        println!("{:?}", request);
+        info!("[INFO] Received a request of type {}", &request.method);
+        if &request.method == "register_worker" {
+            let register_req: RegisterWorkerRequest = serde_json::from_value(request.params)?;
+            if !workers.read().await.contains(&register_req.worker_id) {
+                workers.write().await.insert(register_req.worker_id);
+            } else {
+                // TODO: Build custom Error types to know why the processing fails.
+                anyhow::bail!("This worker ID is already registered!");
+            }
+        }
     } else {
         // It's a notification
         let notification: JsonRpcNotification = serde_json::from_value(message)?;
-        println!("{:?}", notification);
+        if &notification.method == "heartbeat" {} // Implement heartbeat
     }
     Ok(())
 }
--- aether-broker/tests/jrpc_core.rs
+++ aether-broker/tests/jrpc_core.rs
@@ -7,8 +7,18 @@ use aether_broker::state::BrokerState;
 use serde_json::json;
 use tokio::net::TcpStream;
 
+use std::sync::Once;
+static INIT: Once = Once::new();
+
+fn init_tracing() {
+    INIT.call_once(|| {
+        tracing_subscriber::fmt().init();
+    });
+}
+
 #[tokio::test]
 async fn test_parsing() {
+    init_tracing();
     let state = BrokerState::new(10);
     tokio::spawn(create_jrpc_server(state, 6969));
 
@@ -20,6 +30,31 @@ async fn test_parsing() {
         id: "worker1-id1".to_string(),
         method: "dummy_method".to_string(),
         params: json!({"arg1": 2, "arg2": "hola"}),
+    };
+    let body = serde_json::to_string(&request).unwrap();
+    stream
+        .write_all(format!("Content-Length: {}\r\n\r\n{}", body.len(), body).as_bytes())
+        .await
+        .unwrap();
+
+    stream.flush().await.unwrap();
+    tokio::time::sleep(Duration::from_secs(2)).await;
+}
+
+#[tokio::test]
+async fn test_register_worker() {
+    init_tracing();
+    let state = BrokerState::new(10);
+    tokio::spawn(create_jrpc_server(state, 7777));
+
+    tokio::time::sleep(Duration::from_secs(5)).await;
+
+    let mut stream = TcpStream::connect("127.0.0.1:7777").await.unwrap();
+    let request = JsonRpcRequest {
+        jsonrpc: "2.0".to_string(),
+        id: "worker1-id1".to_string(),
+        method: "register_worker".to_string(),
+        params: json!({"worker_id": "test-worker-1"}),
     };
     let body = serde_json::to_string(&request).unwrap();
     stream
