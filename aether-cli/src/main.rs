f3686bf0faf467dbfba71563c1e7343a50a3b58d
NoOPeEKS <arnauapps@gmail.com>
2025-12-25 16:33:02 +0100

    feat(cli): broker and worker start command initialize default broker and worker

--- aether-cli/src/main.rs
+++ aether-cli/src/main.rs
@@ -1,9 +1,19 @@
-use clap::Parser;
 mod commands;
 
 use crate::commands::{BrokerCommands, Cli, Commands, WorkerCommands};
+use aether_broker::DefaultBroker;
+use aether_core::{
+    broker::storage::InMemoryStorage,
+    capabilities::{WorkerCPUArchitecture, WorkerCapabilities},
+    traits::Broker,
+};
+use aether_worker::Worker;
+use clap::Parser;
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt().init();
+
     let cli = Cli::parse();
 
     match cli.command {
@@ -12,9 +22,12 @@ fn main() {
                 api_port,
                 jrpc_port,
             } => {
-                println!(
-                    "Starting http server at 0.0.0.0:{api_port}...\nStarting jrpc server at 0.0.0.0:{jrpc_port}"
-                );
+                let storage = InMemoryStorage::new();
+                let broker = DefaultBroker::new(storage);
+                broker
+                    .run(api_port, jrpc_port)
+                    .await
+                    .expect("Broker should run");
             }
         },
         Commands::Worker { command } => match command {
@@ -22,7 +35,13 @@ fn main() {
                 broker_ip,
                 broker_port,
             } => {
-                println!("Connecting to broker at {broker_ip}:{broker_port}");
+                let worker_capabilities = WorkerCapabilities {
+                    gpu: false,
+                    arch: WorkerCPUArchitecture::X86_64,
+                };
+                let ip = format!("{broker_ip}:{broker_port}");
+                let worker = Worker::new("test-worker-id", &ip, 10, worker_capabilities);
+                worker.run().await.expect("Worker should run");
             }
         },
         Commands::Tui {
